9e3e742f02a9b389b1890254cad8d55031d02a76
theAndrewCline <andrew.cline77@gmail.com>
2017-12-15 10:07:58 -0600

    menu and emails components

---
+++ src/Emails.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Emails extends Component {
+   render() {
+       return (
+            <h1>This should be another box</h1>
+       )
+   } 
+}
---
+++ src/Menu.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Menu extends Component {
+   render() {
+       return (
+            <h1>This should be a box</h1>
+       )
+   } 
+}
