9711db4f771c4eeb51e12c3a8baac299f71d02b0
theAndrewCline <andrew.cline77@gmail.com>
2018-02-07 10:06:56 -0600

    updates to viewer

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -8,7 +8,7 @@ export default class Viewer extends Component {
     render() {
         return (
             <div className='Viewer'>
-                <ViewerHeader tags={this.props.tags} currentMessage={this.props.currentMessage} />
+                <ViewerHeader tags={this.props.tags} currentMessage={this.props.currentMessage} addTagToMessage={this.props.addTagToMessage} />
                 <hr/>
                 <ViewerBody currentMessage={this.props.currentMessage}/>
                 <hr/>
